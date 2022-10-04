dea0fe503b41bd9cc8bcf6bca4fd68fb578e21a5
yura178500 <yura178500@yandex.ru>
2022-10-04 21:08:20 +0300

    Задание 4

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        int agg = 18;
+        if (agg <= 18) {
+            System.out.println("Поздравление с совершеннолетием");
+                }
+        {
+            //Задача 2.1
+            int agg = 7;
+            if (agg <= 7) {
+                System.out.println("Ребенок ходит в школу");
+                        }
+        {
+            //Задача2.2
+            int agg = 18;
+            if (agg <= 18) {
+                System.out.println("Вы закончили школу и можете отправляться в университет");
+        }
+            {
+             //Задача2.3
+                int agg = 24;
+                if (agg <= 24) {
+                    System.out.println("Вы окончил университет и пора искать первую работу");
+            }
+                //Задача3
+                int standingPlace = 120;
+                int place = 60;
+                int passengers = 60;
+                if (passengers < 60) {
+                System.out.println("Сидячих мест нет");
+                    int passengers = 60;
+                    if (passengers >= 60) {
+                        System.out.println("Сидячих мест есть");
+                    }
+
+
+}
