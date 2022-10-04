e2710eb57a9ade6b1401be5865e9fa224b32e1e5
yura178500 <yura178500@yandex.ru>
2022-10-04 22:34:59 +0300

    Задание 4,5 b 6

--- src/Main.java
+++ src/Main.java
@@ -69,6 +69,38 @@ public class Main {
                                     System.out.println("Сидячих мест нет");}
                                 else {System.out.println("Сидячие места есть");}
 
+                                //Задача 6.1
+                            public class Main {
+                                public static void main(String[] args) {
+                                    int agg = 6;
+                                    if (agg <= 2 && agg >=6) {
+                                        System.out.println("нужно ходить в детский сад");
+                                    }
+                                    int agg = 18;
+                                    if (agg <= 7 && agg >=18) {
+                                        System.out.println("нужно ходить в школу");
+                                    }
+                                    int agg = 24;
+                                    if (agg <= 18 && agg >24) {
+                                        System.out.println("нужно ходить в университет");
+                                    } else {System.out.println("нужно ходить на работу");
+                                    }
+                                    //Задача 6.2
+                                    int agg = 5;
+                                    boolean cenNotGo = agg >5;
+                                    if (cenNotGo) {
+                                        System.out.println("не может кататься на аттракционе");
+                                    }
+                                    int agg = 14;
+                                    boolean cenNotGo = agg <= 5 && agg >=14;
+                                    if (cenNotGo) {
+                                        System.out.println("может кататься на аттракционе");
+                                    }
+                                    int agg = 14;
+                                    boolean cenNotGo = agg <= 14;
+                                    if (cenNotGocenNotGo) {
+                                        System.out.println("может кататься без сопровождения взрослого");}
+
 
 
 
