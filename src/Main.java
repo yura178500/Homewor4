97c2e27193db312a2de16ec21102fc181e951925
yura178500 <yura178500@yandex.ru>
2022-10-04 22:01:12 +0300

    Задание 4 и 5

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
+        //Задача 4.1
         int agg = 18;
         if (agg <= 18) {
             System.out.println("Поздравление с совершеннолетием");
                 }
         {
-            //Задача 2.1
+            //Задача 4.2.1
             int agg = 7;
             if (agg <= 7) {
                 System.out.println("Ребенок ходит в школу");
                         }
         {
-            //Задача2.2
+            //Задача 4.2.2
             int agg = 18;
             if (agg <= 18) {
                 System.out.println("Вы закончили школу и можете отправляться в университет");
         }
             {
-             //Задача2.3
+             //Задача 4.2.3
                 int agg = 24;
                 if (agg <= 24) {
                     System.out.println("Вы окончил университет и пора искать первую работу");
             }
-                //Задача3
+                //Задача 4.3
                 int standingPlace = 120;
                 int place = 60;
                 int passengers = 60;
@@ -33,6 +33,45 @@ public class Main {
                     if (passengers >= 60) {
                         System.out.println("Сидячие места есть");
                     }
+                    public class Main {
+                        public static void main(String[] args) {
+                            //Задача 5.1
+                            int agg = 18;
+                            if (agg <= 18) {
+                                System.out.println("Поздравление с совершеннолетием");
+                            }else {System.out.println("Cовершеннолетия ещё не наступил");
+                        }
+                            // Задача 5.2
+                            int agg = 7;
+                            if (agg > 7) {
+                                System.out.println("Ребенок не ходит в школу");
+                            }else {System.out.println("Ребенок ходит в школу");
+                            }
+                                                        int agg = 18;
+                            if (agg > 18) {
+                                System.out.println("Вы не закончили школу");
+                            } else { System.out.println("Вы закончили школу и можете отправляться в университет");
+                            }
+                            int agg = 24;
+                            if (agg > 24) {
+                                System.out.println("Вы не закончили университет");
+                            } else {System.out.println("Вы закончили в университет и можете работатиь");
+                            }
+                            // Задание 5.3
+                            int standingPlace = 120;
+                            int place = 60;
+                            int passengers = 60;
+                            if (passengers >= 60) {
+                                System.out.println("Сидячие места есть");}
+                            else {System.out.println("Сидячих места нет");}
+                                int passengers = 60;
+                                if (passengers < 60) {
+                                    System.out.println("Сидячих мест нет");}
+                                else {System.out.println("Сидячие места есть");}
+
+
+
+
 
 
 }
